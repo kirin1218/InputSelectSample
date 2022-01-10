ad81af37a7397df39e3d21622b3e391f0101606d
Kiri <kirin1218@outlook.jp>
2022-01-11 07:46:05 +0900

    入力選択コントロール作成中

--- App.tsx
+++ App.tsx
@@ -1,10 +1,67 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInputProps, View } from 'react-native';
+import RNPickerSelect, { PickerSelectProps } from 'react-native-picker-select';
+import {Input} from 'react-native-elements'
+import { Ionicons } from '@expo/vector-icons';
+import { patchWebProps } from 'react-native-elements/dist/helpers';
+
+
+//  picker-selectでinput部を隠して、ボタン部分のみを実装する
+const SelectView: React.FC<PickerSelectProps> = (props) => {
+  return (
+      <View>
+        <RNPickerSelect
+          style={{
+            inputIOS:{
+              opacity:0
+            }
+          }}
+          Icon={() => {
+            return <Ionicons name="md-arrow-down" size={24} color="gray" />;
+          }}
+          {...props}
+        />
+      </View>
+  )
+}
+
+type InputSelectProps = {
+  inputProps: TextInputProps;
+  selectProps: PickerSelectProps;
+};
+
+//  直接入力と選択が可能なコントロール
+//  内部的には入力コントトールのrightIconにselect-pickerを埋め込む
+const InputSelect: React.FC<InputSelectProps> = (props) => {
+  //  値によって、InputのrightIconを切り替える
+  const rightArea = (
+    <View style={{flexDirection:'row',  width:40, justifyContent:'center'}}>
+      { props.inputProps?.value ? <Text>☒</Text> : null }
+      { props.selectProps?.items ? <SelectView {...props.selectProps} /> : undefined}
+    </View>
+  )
+
+  return (
+    <Input
+      {...props.inputProps}
+      rightIcon = {rightArea}
+    />
+  )
+}
 
 export default function App() {
+  const [textVal,setTextVal] = useState<string>("1");
+  const items=[
+              { label: 'Football2', value: 'football' },
+              { label: 'Baseball2', value: 'baseball' },
+              { label: 'Hockey2', value: 'hockey' },
+          ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <InputSelect inputProps={
+        {value: textVal,onChangeText:setTextVal}
+      } selectProps={{onValueChange:(val:string) => {setTextVal(val)}, items:items}} />
       <StatusBar style="auto" />
     </View>
   );
